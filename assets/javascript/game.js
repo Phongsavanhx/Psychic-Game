5f64895397d7b3a0dcc44f93b8f11cd8211aa282
Sushi Sama <Phongsavanhx@gmail.com>
2018-10-18 17:11:56 -0700

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+var computerChoices = 
+["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var winsCounter = 0;
+var lossCounter = 0;
+var amountofGuesses = 9;
