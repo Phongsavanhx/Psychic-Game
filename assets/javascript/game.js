2e8c2bc4e98e0dd7484ab8525e4c45bd96cd63b6
Sushi Sama <Phongsavanhx@gmail.com>
2018-10-18 23:03:11 -0700

    finished work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,44 @@ var computerChoices =
 var winsCounter = 0;
 var lossCounter = 0;
 var amountofGuesses = 9;
+var guessChoices = [];
+
+document.onkeyup = function(event) {
+
+var userGuess = event.key;
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var options = 
+["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+                 
+    
+    if (options.indexOf(userGuess) > -1) {
+    
+    if (userGuess === computerGuess) {
+        winsCounter ++;
+        amountofGuesses = 9;
+        guessChoices = [];
+        }
+    
+    if (userGuess != computerGuess) {
+        amountofGuesses --;
+        guessChoices.push(userGuess);
+        }
+    
+    if (amountofGuesses === 0) {
+        amountofGuesses = 9;
+        lossCounter ++;
+        guessChoices = [];
+    
+        }
+    
+    var html = 
+        "<h1> The Psychic Game </h1>" +
+        "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + winsCounter + "</p>" +
+        "<p>Losses: " + lossCounter + "</p>" +
+        "<p>Guesses Left: " + amountofGuesses + "</p>" +
+        "<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+        
+        document.querySelector("#game").innerHTML = html;
+    
+    }};
